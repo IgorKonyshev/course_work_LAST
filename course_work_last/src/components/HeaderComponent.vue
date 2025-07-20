e04c8d567c91d63d06cae709cea417bc36d56d24
IgorKonyshev <snpport123@gmail.com>
2025-07-20 10:39:51 +0300

    делаем новый сайт типо магазинчик

---
+++ course_work_last/src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template> <header>
+
+</header></template>
+
+<script>export default {
+    name: "HeaderComponent"
+};</script>
+
+<style>header{
+
+    background: #42b983;
+    color: white;
+    padding: 15px;
+    text-align: center;
+}</style>
